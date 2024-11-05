58aab0046a7b31964e8b33c34cbff32e30fa5e58
Justin Marney <gotascii@gmail.com>
2024-11-04 16:03:44 -0800

    go-git sucks at detecting case-only renames

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	git "github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
 )
 
+// Helper function to find the case-only renamed file
+func findCaseOnlyRename(deletedFile string, status git.Status) bool {
+	for otherFile, otherStat := range status {
+		// if the file has the same case-insensitive name as the deleted one
+		// && it isn't the same file
+		// && the file we're looking at is new
+		// then it's a case-only rename
+		if strings.EqualFold(deletedFile, otherFile) && deletedFile != otherFile && otherStat.Worktree == git.Untracked {
+			return true
+		}
+	}
+	return false
+}
+
 func execute(cmd *cobra.Command, args []string) error {
 	path, _ := cmd.Flags().GetString("path")
-	msg, _ := cmd.Flags().GetString("msg")
 
 	r, err := git.PlainOpen(path)
 	if err != nil {
@@ -18,9 +32,37 @@ func execute(cmd *cobra.Command, args []string) error {
 
 	w, _ := r.Worktree()
 
-	status, _ := w.Status()
+	// Open the index directly
+	index, err := r.Storer.Index()
+	if err != nil {
+		return err
+	}
+
+	// Detect case-only renames
+	status, err := w.Status()
+	if err != nil {
+		return err
+	}
+
 	if status.IsClean() {
 		return nil
+	}
+
+	for file, stat := range status {
+		if stat.Worktree == git.Deleted {
+			if findCaseOnlyRename(file, status) {
+				// Remove the old file from the index without affecting the working tree
+				_, err := index.Remove(file)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// Save changes to the index
+	if err := r.Storer.SetIndex(index); err != nil {
+		return err
 	}
 
 	err = w.AddGlob(".")
@@ -28,7 +70,11 @@ func execute(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	_, err = w.Commit(msg, &git.CommitOptions{})
+	msg, _ := cmd.Flags().GetString("msg")
+	_, err = w.Commit(msg, &git.CommitOptions{
+		All:               true,
+		AllowEmptyCommits: false,
+	})
 	if err != nil {
 		return err
 	}
